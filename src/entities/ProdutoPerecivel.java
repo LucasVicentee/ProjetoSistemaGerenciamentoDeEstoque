5eef2a74a6e45d9463f016cb13db0726e32a181d
LucasVicentee <lh201612@gmail.com>
2025-07-29 20:42:10 -0300

    Criando a classe ProdutoPerecivel

--- src/entities/ProdutoPerecivel.java
+++ src/entities/ProdutoPerecivel.java
@@ -5,5 +5,20 @@ import java.util.Date;
 public class ProdutoPerecivel extends Produto {
     private Date dataValidade;
 
+    public ProdutoPerecivel() {
 
+    }
+
+    public ProdutoPerecivel(Integer id, String nome, double preco, int quantidade, Date dataFabricacao, String fabricante, Date dataValidade) {
+        super(id, nome, preco, quantidade, dataFabricacao, fabricante);
+        this.dataValidade = dataValidade;
+    }
+
+    public Date getDataValidade() {
+        return dataValidade;
+    }
+
+    public void setDataValidade(Date dataValidade) {
+        this.dataValidade = dataValidade;
+    }
 }
