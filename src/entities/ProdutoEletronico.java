a515c01228de70da403abcf50bf77e6c6b822c24
LucasVicentee <lh201612@gmail.com>
2025-07-29 20:45:56 -0300

    Criando a classe ProdutoEletronico

--- src/entities/ProdutoEletronico.java
+++ src/entities/ProdutoEletronico.java
@@ -1,4 +1,34 @@
 package entities;
 
-public class ProdutoEletronico {
+import java.util.Date;
+
+public class ProdutoEletronico extends Produto{
+    private int garantiaMeses;
+    private String voltagem;
+
+    public ProdutoEletronico() {
+
+    }
+
+    public ProdutoEletronico(Integer id, String nome, double preco, int quantidade, Date dataFabricacao, String fabricante, int garantiaMeses, String voltagem) {
+        super(id, nome, preco, quantidade, dataFabricacao, fabricante);
+        this.garantiaMeses = garantiaMeses;
+        this.voltagem = voltagem;
+    }
+
+    public int getGarantiaMeses() {
+        return garantiaMeses;
+    }
+
+    public void setGarantiaMeses(int garantiaMeses) {
+        this.garantiaMeses = garantiaMeses;
+    }
+
+    public String getVoltagem() {
+        return voltagem;
+    }
+
+    public void setVoltagem(String voltagem) {
+        this.voltagem = voltagem;
+    }
 }
