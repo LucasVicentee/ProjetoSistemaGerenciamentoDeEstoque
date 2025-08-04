470895580051347c4c7874c3e7c812ce9752438e
LucasVicentee <lh201612@gmail.com>
2025-08-04 16:28:56 -0300

    Iniciando o DAO do produto perecivel

--- src/DAO/ProdutoPerecivelDAO.java
+++ src/DAO/ProdutoPerecivelDAO.java
@@ -1,5 +1,12 @@
 package DAO;
 
+import entities.ProdutoPerecivel;
+
 public class ProdutoPerecivelDAO {
 
+    public void cadastrarProdutoPerecivel(ProdutoPerecivel pp) {
+
+        String sqlProduto = "INSERT INTO produto (nome, preco, quantidade, data_fabricacao, fabricante) VALUES (?, ?, ?, ?, ?)";
+        String sqlProdutoPerecivel = "INSERT INTO produto_perecivel (data_validade,     )"
+    }
 }
