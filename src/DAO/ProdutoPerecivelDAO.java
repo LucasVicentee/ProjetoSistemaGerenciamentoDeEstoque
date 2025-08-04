b40c2944041caf3106e97a628c63432d719c85da
LucasVicentee <lh201612@gmail.com>
2025-08-04 20:36:29 -0300

    Criando a funcionalidade de cadastrar um produto perecível no sistema

--- src/DAO/ProdutoPerecivelDAO.java
+++ src/DAO/ProdutoPerecivelDAO.java
@@ -1,6 +1,9 @@
 package DAO;
 
+import connection.Conexao;
 import entities.ProdutoPerecivel;
+
+import java.sql.*;
 
 public class ProdutoPerecivelDAO {
 
@@ -9,6 +12,33 @@ public class ProdutoPerecivelDAO {
         String sqlProduto = "INSERT INTO produto (nome, preco, quantidade, data_fabricacao, fabricante) VALUES (?, ?, ?, ?, ?)";
         String sqlProdutoPerecivel = "INSERT INTO produto_perecivel (data_vencimento, tipo_produto, peso_gramas, temperatura_armazenamento) VALUES (?, ?, ?, ?)";
 
+        try (Connection conn = Conexao.getConexao();
+             PreparedStatement psProduto = conn.prepareStatement(sqlProduto, Statement.RETURN_GENERATED_KEYS))
+        {
+            psProduto.setString(1, pp.getNome());
+            psProduto.setDouble(2, pp.getPreco());
+            psProduto.setInt(3, pp.getQuantidade());
+            psProduto.setDate(4, new java.sql.Date(pp.getDataFabricacao().getTime()));
+            psProduto.setString(5, pp.getFabricante());
 
+            try (ResultSet rs = psProduto.getGeneratedKeys()) {
+                if (rs.next()) {
+                    int idGerado = rs.getInt(1);
+                    pp.setId(idGerado);
+                }
+            }
+
+            try (PreparedStatement psProdutoPerecivel = conn.prepareStatement(sqlProdutoPerecivel)) {
+                psProdutoPerecivel.setDate(1, new java.sql.Date(pp.getDataVencimento().getTime()));
+                psProdutoPerecivel.setString(2, pp.getTipoProduto());
+                psProdutoPerecivel.setDouble(3, pp.getPesoGramas());
+                psProdutoPerecivel.setString(4, pp.getTemperaturaArmazenamento());
+
+                System.out.println("Produto perecível cadastrado com sucesso!");
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
