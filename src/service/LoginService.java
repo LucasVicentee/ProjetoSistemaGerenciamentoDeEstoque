623e366a2cade863bcbbb6866ef06c129c6245d7
LucasVicentee <lh201612@gmail.com>
2025-08-14 16:13:43 -0300

    Adicionando comentários a classe LoginService do pacote service

--- src/service/LoginService.java
+++ src/service/LoginService.java
@@ -6,21 +6,21 @@ import entities.Login;
 
 public class LoginService {
 
-    private LoginDAO loginDAO = new LoginDAO();
+    private LoginDAO loginDAO = new LoginDAO(); //Criando o objeto LoginDAO do pacote DAO
 
     // Cadastro de usuário
-    public void cadastrarUsuario(String nome, String email, String senha_hash) {
+    public void cadastrarUsuario(String nome, String email, String senha_hash) { //Criação do método com a passagem dos dados
 
         Login usuario = new Login();
         usuario.setNome(nome);
         usuario.setEmail(email);
         usuario.setSenha_hash(senha_hash);
-        loginDAO.cadastrarUsuario(usuario);
+        loginDAO.cadastrarUsuario(usuario); //Chama o método do LoginDAO para realmente inserir esse usuário no banco
     }
 
     // Login do usuário
-    public Login verificarUsuarioOuEmail(String usuarioOuEmail, String senha) {
+    public Login verificarUsuarioOuEmail(String usuarioOuEmail, String senha) { //Método que verifica se possuí ou não um usuário com um usuário ou email cadastrados
 
-        return loginDAO.verificarUsuarioOuEmail(usuarioOuEmail, senha);
+        return loginDAO.verificarUsuarioOuEmail(usuarioOuEmail, senha); //Retorna se tem ou não este usuário no sistema cadastrado
     }
 }
